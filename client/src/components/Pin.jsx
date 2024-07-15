64c761d0009fdeaa1020cd594935c60d3415a68b
Jasnoor Maan <jasnoorm0001@gmail.com>
2024-07-15 11:11:22 +0530

    check

--- client/src/components/Pin.jsx
+++ client/src/components/Pin.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Pin = ({ pinSize }) => {
-  return <div className={`${pinSize} `}></div>;
+  return (
+    <>
+      <div className={`${pinSize} `}></div>
+    </>
+  );
 };
 
 export default Pin;
